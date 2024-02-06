ce696a5d6415f25dfc1933a04a49c24f6af73ee8
Lenovo <saianuraag.m@gmail.com>
2024-02-05 19:14:49 -0600

    code fix

--- src/app/navbar/portfolio/portfolio.component.ts
+++ src/app/navbar/portfolio/portfolio.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Project } from 'src/app/_models/Project';
 
 @Component({
   selector: 'app-portfolio',
@@ -7,6 +8,15 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent {
+  project: Project = {
+    id: 0,
+    name: 'Sample Angular App',
+    summary: 'An Angular application',
+    description: '',
+    projectLink: '',
+    pictures: [],
+    tags: ['Angular','Selenium']
+  }
   constructor(private titleservice: Title){
     this.titleservice.setTitle('SAM-Portfolio')
   }
